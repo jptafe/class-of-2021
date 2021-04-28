fbde151b121e08b3024e0f3175c7c4735f0229ca
CameronDavies227 <41108322+CameronDavies227@users.noreply.github.com>
2021-04-28 17:09:57 +1000

    Cam's assignment start

---
+++ Davies_470957194_PHEM/PHEM.js
@@ -0,0 +1,15 @@
+function swapBoxes() {
+    console.log('meow');
+
+
+
+
+  
+    if(bone.style.background=="green"){
+        btwo.style.background="green"
+        bone.style.background="blue"
+    }else {
+        btwo.style.background="blue"
+        bone.style.background="green"
+    }
+}
